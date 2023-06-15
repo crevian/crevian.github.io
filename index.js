3bd3dadc1395754567f9c04530b89315bd7f92b6
Vlad Crevian <vlad.crevian@gmail.com>
2023-06-15 18:59:37 +0300

    updated menu

--- index.js
+++ index.js
@@ -18,8 +18,17 @@ function hideAllPages() {
 
 function displayPage(id) {
   hide(activePage);
+  document
+    .querySelector(`#top-menu-bar a[data-page="${activePage}"]`)
+    .classList.remove("active");
   show(id);
   activePage = id;
+  console.warn(id, `#top-menu-bar a[data-page="${id}"]`);
+  console.warn(document.querySelector(`#top-menu-bar a[data-page="${id}"]`));
+
+  document
+    .querySelector(`#top-menu-bar a[data-page="${id}"]`)
+    .classList.add("active");
 }
 
 displayPage(activePage);
