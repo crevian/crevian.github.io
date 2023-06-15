3209b6d1297f1d88c61ded1707e275c9bbd9f2cb
Vlad Crevian <vlad.crevian@gmail.com>
2023-06-15 18:25:32 +0300

    add active page

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-function hideAllPages() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("languages").style.display = "none";
-  document.getElementById("projects").style.display = "none";
+var activePage = "home";
+
+function hide(id) {
+  document.getElementById(id).style.display = "none";
 }
 
-function displayPage(id) {
-  hideAllPages();
+function show(id) {
   var page = document.getElementById(id);
   page.style.display = "block";
 }
 
-displayPage("home");
+function hideAllPages() {
+  hide("home");
+  hide("skills");
+  hide("languages");
+  hide("projects");
+}
+
+function displayPage(id) {
+  hide(activePage);
+  show(id);
+  activePage = id;
+}
+
+displayPage(activePage);
